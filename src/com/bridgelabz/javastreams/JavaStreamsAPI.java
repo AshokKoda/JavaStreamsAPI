23bad6aabf97a9f81bea7d42a3b784101f69903c
AshokKoda <a.koda59@gmail.com>
2022-04-20 21:20:44 +0530

    Added:Welcome to java streams API

---
+++ src/com/bridgelabz/javastreams/JavaStreamsAPI.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.javastreams;
+
+public class JavaStreamsAPI {
+
+	public static void main(String[] args) {
+		System.out.println("************ Welcome to Java Streams API ************");
+
+	}
+
+}
