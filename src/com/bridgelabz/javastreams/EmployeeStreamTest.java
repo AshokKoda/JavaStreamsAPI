f1e26cf84deca960c6175fc12d6a34a1530ba0d7
AshokKoda <a.koda59@gmail.com>
2022-04-20 21:42:25 +0530

    Added:UC2.2 - Transform each element to double

--- src/com/bridgelabz/javastreams/EmployeeStreamTest.java
+++ src/com/bridgelabz/javastreams/EmployeeStreamTest.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.javastreams;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -22,5 +25,22 @@ public class EmployeeStreamTest {
 		};
 
 		empStream.forEach(empConsumer);
+	}
+
+	@Test
+	public void streamTransform() {
+
+		List<Integer> intList = new LinkedList<>();
+
+		for (int i = 0; i < 5; i++) {
+			int num = (int) (Math.random() * 90 + 10);
+			intList.add(num);
+		}
+
+		Stream<Integer> intStream = intList.stream();
+
+		Function<Integer, Double> doubleMap = n -> n.doubleValue();
+
+		intStream.map(doubleMap).forEach(n -> System.out.println(n));
 	}
 }
