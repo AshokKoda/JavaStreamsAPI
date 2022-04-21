6037dc9f7305f1bca39f523392a2fd8a2d736c75
AshokKoda <a.koda59@gmail.com>
2022-04-21 10:29:10 +0530

    Added:UC2.6 -  find max and min from stream

--- src/com/bridgelabz/javastreams/EmployeeStreamTest.java
+++ src/com/bridgelabz/javastreams/EmployeeStreamTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.javastreams;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -100,6 +101,25 @@ public class EmployeeStreamTest {
 		int a = stream.filter(n -> checkPrime(n)).findFirst().get();
 
 		System.out.println("First prime number is : " + a);
+	}
+
+	@Test
+	public void streamMinMax() {
+		List<Integer> intList = new LinkedList<>();
+
+		for (int i = 0; i < 5; i++) {
+			int num = (int) (Math.random() * 90 + 10);
+			intList.add(num);
+		}
+		System.out.println(intList);
+
+		int minNum = intList.stream().filter(n -> n % 2 == 0).min(Comparator.comparing(n -> n)).get();
+
+		int maxNum = intList.stream().filter(n -> n % 2 == 0).max(Comparator.comparing(n -> n)).get();
+
+		System.out.println("Minimum even is : " + minNum);
+		System.out.println("Maximum even is : " + maxNum);
+
 	}
 
 	public boolean checkPrime(int n) {
